70c5485540a315ca6796d13a96bc3778367fb7a1
jme418 <jkingmues@gmail.com>
2026-02-28 14:46:38 +0100

    feat: add tabbed navigation with three tabs and explore container component

--- src/app/features/tab2/tab2.page.ts
+++ src/app/features/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { ExploreContainerComponent } from '../../shared/components/explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +9,4 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
-
-  constructor() {}
-
 }
